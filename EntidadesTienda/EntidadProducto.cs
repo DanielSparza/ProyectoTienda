313b3bde69f1e74f7b76e64ca0eafd5958374daa
Daniel Esparza <danielesparzza@gmail.com>
2020-09-14 09:22:55 -0500

    Se agregó la clase Manejador Productos y las referencias a Entidades y Acceso a Datos

--- EntidadesTienda/EntidadProducto.cs
+++ EntidadesTienda/EntidadProducto.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EntidadesTienda
+﻿namespace EntidadesTienda
 {
-    class EntidadProducto
+    public class EntidadProducto
     {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+
+        public EntidadProducto(int idproducto, string nombre, string descripcion, double precio)
+        {
+            IdProducto = idproducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
     }
 }
---
+++ ManejadorTienda/ManejadorProductos.cs
@@ -0,0 +1,38 @@
+﻿using EntidadesTienda;
+using AccesoDatosTienda;
+using System.Data;
+
+namespace ManejadorTienda
+{
+    public class ManejadorProductos
+    {
+        ConexionTienda ct = new ConexionTienda();
+
+        //Guardar producto
+        public string Guardar(EntidadProducto producto)
+        {
+            return ct.Comando(string.Format("insert into producto values" +
+                "(null, '{0}', '{1}', {2})", producto.Nombre, producto.Descripcion, producto.Precio));
+        }
+
+        //Borrar producto
+        public string Borrar(EntidadProducto producto)
+        {
+            return ct.Comando(string.Format("delete from producto where Id_Producto={0}", producto.IdProducto));
+        }
+
+        //Modificar producto
+        public string Modificar(EntidadProducto producto, string id2)
+        {
+            return ct.Comando(string.Format("update producto set Nombre='{0}'," +
+                " Descripcion='{1}', Precio={2} where Id_Producto={3}",
+                producto.Nombre, producto.Descripcion, producto.Precio, id2));
+        }
+
+        //Mostrar Informacion
+        public DataSet Listado(string q, string tabla)
+        {
+            return ct.Mostrar(q, tabla);
+        }
+    }
+}
